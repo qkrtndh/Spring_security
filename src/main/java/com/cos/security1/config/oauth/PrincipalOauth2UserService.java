944e0ec5df73f4fd890d5bc088ebb5271b47f4b7
qkrtndh <qkrtndh5@naver.com>
2021-07-23 14:18:33 +0900

    네이버 로그인 완료

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.config.oauth.provider.FacebookUserInfo;
 import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
 import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
@@ -43,8 +46,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		} else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
 			System.out.println("페이스북 요청");
 			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+		}else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			System.out.println("네이버 요청");
+			oAuth2UserInfo = new NaverUserInfo((Map)oauth2User.getAttributes().get("response"));
 		}else {
-			System.out.println("구글이나 페이스북만 지원");
+			System.out.println("구글이나 페이스북, 네이버만 지원");
 		}
 
 		String provider = oAuth2UserInfo.getProvider();
---
+++ src/main/java/com/cos/security1/config/oauth/provider/NaverUserInfo.java
@@ -0,0 +1,37 @@
+package com.cos.security1.config.oauth.provider;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuth2UserInfo{
+
+	private Map<String,Object> attributes;//getAttributes
+	
+	public NaverUserInfo(Map<String,Object> attributes) {
+		this.attributes=attributes;
+	}
+	
+	@Override
+	public String getProviderId() {
+		// TODO Auto-generated method stub
+		return (String)attributes.get("id");
+	}
+
+	@Override
+	public String getProvider() {
+		// TODO Auto-generated method stub
+		return "naver";
+	}
+
+	@Override
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return (String)attributes.get("email");
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return (String)attributes.get("name");
+	}
+
+}
